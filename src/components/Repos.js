02d2f82ce0b3cee9f4145f00d8c89cf43773db32
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-07 15:50:31 +0700

    :necktie: calculate the most used programming language
    put it on the chart component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,18 +1,48 @@
 import React, { useContext } from "react";
 import styled from "styled-components";
 import { GithubContext } from "../context/context";
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
+import { Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 
 const Repos = () => {
   const { repos } = useContext(GithubContext);
 
-  return <ExampleChart />;
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+
+    if (!language) return total;
+
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+
+    return total;
+  }, {});
+
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <Pie3D data={languages} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
   display: grid;
   justify-items: center;
   gap: 2rem;
+
   @media (min-width: 800px) {
     grid-template-columns: 1fr 1fr;
   }
@@ -24,9 +54,11 @@ const Wrapper = styled.div`
   div {
     width: 100% !important;
   }
+
   .fusioncharts-container {
     width: 100% !important;
   }
+
   svg {
     width: 100% !important;
     border-radius: var(--radius) !important;
