5db2e3f2519f38496e797f00aedfbba814160bd4
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-07 17:20:35 +0700

    :alembic: setup the search components
    on-going

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { MdSearch } from "react-icons/md";
 import { GithubContext } from "../context/context";
 
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (user) {
+      // todo => come back here again
+    }
+  };
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input
+              type="text"
+              placeholder="enter github user"
+              value={user}
+              onChange={({ target }) => setUser(target.value)}
+            />
+            <button type="submit">search</button>
+          </div>
+        </form>
+
+        <h3>requests : 60 / 60</h3>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
   position: relative;
   display: grid;
   gap: 1rem 1.75rem;
+
   @media (min-width: 768px) {
     grid-template-columns: 1fr max-content;
     align-items: center;
