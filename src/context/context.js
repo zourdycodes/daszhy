0dd0819787cf0297e175ebf639863c5fa95668c6
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-07 21:52:17 +0700

    :recycle: refactor the fetch data with Promise.allSettled
    done

--- src/context/context.js
+++ src/context/context.js
@@ -31,38 +31,38 @@ export const GithubProvider = ({ children }) => {
       setGithubUser(response.data);
       const { login, followers_url } = response.data;
 
-      // repo
-      await axios(`${rootUrl}/users/${login}/repos?per_page=100`).then(
-        (response) => {
-          setRepos(response.data);
-        }
-      );
+      // // repo
+      // await axios(`${rootUrl}/users/${login}/repos?per_page=100`).then(
+      //   (response) => {
+      //     setRepos(response.data);
+      //   }
+      // );
 
-      // follower
-      await axios(`${followers_url}?per_page=100`).then((response) => {
-        setFollowers(response.data);
-      });
+      // // follower
+      // await axios(`${followers_url}?per_page=100`).then((response) => {
+      //   setFollowers(response.data);
+      // });
 
-      // refactoring
-      // await Promise.allSettled([
-      //   axios(`${rootUrl}/users/${login}/repos?per_page=100`),
-      //   axios(`${followers_url}?per_page=100`),
-      // ])
-      //   .then((results) => {
-      //     const [repos, followers] = results;
-      //     const status = "fulfilled";
+      //* refactoring
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ])
+        .then((results) => {
+          const [repos, followers] = results;
+          const status = "fulfilled";
 
-      //     if (repos.status === status) {
-      //       setRepos(repos.value.data);
-      //     }
+          if (repos.status === status) {
+            setRepos(repos.value.data);
+          }
 
-      //     if (followers.status === status) {
-      //       setFollowers(followers.value.data);
-      //     }
-      //   })
-      //   .catch((err) => {
-      //     throw new Error(err);
-      //   });
+          if (followers.status === status) {
+            setFollowers(followers.value.data);
+          }
+        })
+        .catch((err) => {
+          throw new Error(err);
+        });
     } else {
       toggleError(true, "there is no user with that username!");
     }
